b81558072775dcfe0b283bc601505637e87316cd
Andrew Moscardino <andrew@moscardino.net>
2019-10-28 15:15:16 -0400

    1.4.1 - Refactored shared components to use new partial class code-behind pattern

---
+++ StandUpCore/Shared/MainLayout.razor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using StandUpCore.Services;
+
+namespace StandUpCore.Shared
+{
+    public partial class MainLayout
+    {
+        [Inject]
+        public ConfigurationService ConfigService { get; set; }
+
+        private string AppVersion { get; set; }
+        private string BlazorVersion { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            AppVersion = await ConfigService.GetValueAsync("AppVersion");
+            BlazorVersion = await ConfigService.GetValueAsync("BlazorVersion");
+
+            this.StateHasChanged();
+        }
+    }
+}
